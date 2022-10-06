2bcaed632da0aabb5bdc04d770ade543edf34f0f
Your Name <you@example.com>
2022-10-06 18:44:57 +0500

    edit timer and start button functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
-import React, { useRef, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
-import {Box, Button, Chip, Grid, Typography} from "@mui/material";
-import Radio from "@mui/material/Radio";
-import RadioGroup from "@mui/material/RadioGroup";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import FormControl from "@mui/material/FormControl";
-import FormLabel from "@mui/material/FormLabel";
-import Timer from "./Timer.js";
+import { Box, Button, Chip, Grid, Typography, TextField } from "@mui/material";
 
 function App() {
   let [questions, setQuestions] = useState([
@@ -80,62 +74,42 @@ function App() {
   let [indexNumber, setIndexNumber] = useState(0);
   let [score, setScore] = useState(0);
   let [showResult, setShowResult] = useState(false);
+  let [showQuestions, setShowQuestions] = useState(false)
 
-  // let [timerMinutes, setTimerMinutes] = useState(3);
-  // let [timerSeconds, setTimerSeconds] = useState(10);
-
-  // let interval;
-
-  // const startTimer = () => {
-  //   interval = setInterval(() => {
-  //     setTimerSeconds(timerSeconds - 1);
-  //     if (timerSeconds < 0) {
-  //       setTimerMinutes(timerMinutes - 1);
-  //       setTimerSeconds(10);
-  //     }
-  //     else if(timerMinutes==0){
-  //       clearInterval(interval)
-  //     }
-  //   }, 10);
-  // };
-
-  // useEffect(() => {
-  //   startTimer();
-  //   return () => {
-  //     clearInterval(interval);
-  //   };
-  // });
-
-  let [seconds, setSeconds] = useState(59);
-  let [minutes, setMinutes] = useState(4);
+  let [miliSec, setMiliSec] = useState(0);
+  let [sec, setSec] = useState(4);
+  let [min, setMin] = useState(0);
 
   let timer;
 
   useEffect(() => {
     timer = setInterval(() => {
-      setSeconds(seconds - 1);
+      setMiliSec(miliSec - 1);
 
-      if (seconds === 0) {
-        setMinutes(minutes - 1);
-        setSeconds(59);
+      if (miliSec === 0) {
+        setSec(sec - 1);
+        setMiliSec(59);
       }
-      else if(minutes === 0 && seconds === 0){
-        clearInterval(timer)
-      }
-    }, 10);
+    }, 5);
 
     return () => clearInterval(timer);
   });
 
-  // let [second, setSecond]= useState();
-  // let interval;
+  let start = () => {
+    setMiliSec(59);
+    setSec(4);
+    setShowQuestions(true)
+  };
+  let stop = () => {
+    clearInterval(timer)
+    setShowResult(true)
+  };
+  useEffect(() => {
+    if (sec === 0 && miliSec === 0) {
+      stop();
+    }
+  })
 
-  // const startTimer = ()=>{
-  //   interval = setInterval(()=>{
-  //     const seconds = 20;
-
-  //   })
-  // }
 
   let checkQuestion = (a, b) => {
     if (a === b) {
@@ -152,103 +126,137 @@ function App() {
 
   return (
     <div style={{ backgroundColor: "lightBlue", height: "100vh" }}>
-      {showResult ? (
-        <>
-          <h1>Total Question : {questions.length}</h1>
-          <h1>No. of questions attempt : {questions.length}</h1>
-          <h1>
-            Correct Answer : {score} / {questions.length}
-          </h1>
-          <h1>Percentage : {Math.round((score * 100) / questions.length)}%</h1>
-        </>
-      ) : (
-        <Box
-          id="main"
-          sx={{
-            bgcolor: "white",
-            boxShadow: "0 10px 15px rgba(0,0,0,0.25), 0 0 6px rgba(0,0,0,0.22)",
-          }}
-        >
-          <button>Start</button>
-          {/* Time remaining */}
-          <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
-            <Chip label={`${minutes} : ${seconds}`}></Chip>
-          </Box>
-          <hr />
+      {showQuestions ?
+        (showResult ?
+          (
+            <>
+              <h1>Total Question : {questions.length}</h1>
+              {/* <h1>No. of questions attempt : {questions.length}</h1> */}
+              <h1>
+                Correct Answer : {score} / {questions.length}
+              </h1>
+              <h1>Percentage : {Math.round((score * 100) / questions.length)}%</h1>
+            </>
+          ) :
+          (
+            // Main 
+            <Box
+              id="main"
+              sx={{
+                bgcolor: "white",
+                boxShadow: "0 10px 15px rgba(0,0,0,0.25), 0 0 6px rgba(0,0,0,0.22)",
+              }}
+            >
+              <button>Start</button>
+              {/* Time remaining */}
+              <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
+                <Chip label={`${min} : ${sec}`}></Chip>
+              </Box>
+              <hr />
 
-          {/* Question */}
-          <Box mt={5}>
-            <Typography variant="h5" sx={{ fontWeight: "bold" }}>
-              {questions[indexNumber].question}
-            </Typography>
-          </Box>
-
-          {/* Answer */}
-          <Box mt={4}>
-            <Grid container>
-              {questions[indexNumber].options.map((x, i) => (
-                <Grid item xs={12} key={i}>
-                  <Typography
-                    mb={2}
-                    id="options"
-                    sx={{ cursor: "pointer" }}
-                    onClick={() =>
-                      checkQuestion(x, questions[indexNumber].correctAns)
-                    }
-                  >
-                    {`${i + 1}. ${x}`}{" "}
-                  </Typography>
-                </Grid>
-              ))}
-            </Grid>
-          </Box>
-
-          <hr />
-
-          <Box>
-            <Grid container>
-              {/* No of questions display */}
-              <Grid item xs={7} sm={8} md={6}>
-                <Typography variant="h6" color="primary">
-                  {indexNumber + 1} of {questions.length} Questions
+              {/* Question */}
+              <Box mt={5}>
+                <Typography variant="h5" sx={{ fontWeight: "bold" }}>
+                  {questions[indexNumber].question}
                 </Typography>
-              </Grid>
-              {/* Next button */}
-              <Grid item xs={5} sm={4} md={6} align="right">
-                <Button
-                  id="next"
-                  my={5}
-                  color="primary"
-                  variant="contained"
-                  onClick={next}
-                >
-                  Next Quiz
-                </Button>
-              </Grid>
-            </Grid>
-          </Box>
+              </Box>
 
-          {/* Score Display on each question */}
-          <Box mt={4}>
-            <Typography align="center" variant="h5">
-              Score: {score}
-            </Typography>
-          </Box>
-        </Box>
-      )}
+              {/* Answer */}
+              <Box mt={4}>
+                <Grid container>
+                  {questions[indexNumber].options.map((x, i) => (
+                    <Grid item xs={12} key={i}>
+                      <Typography
+                        mb={2}
+                        id="options"
+                        sx={{ cursor: "pointer" }}
+                        onClick={() =>
+                          checkQuestion(x, questions[indexNumber].correctAns)
+                        }
+                      >
+                        {`${i + 1}. ${x}`}{" "}
+                      </Typography>
+                    </Grid>
+                  ))}
+                </Grid>
+              </Box>
+
+              <hr />
+
+              <Box>
+                <Grid container>
+                  {/* No of questions display */}
+                  <Grid item xs={7} sm={8} md={6}>
+                    <Typography variant="h6" color="primary">
+                      {indexNumber + 1} of {questions.length} Questions
+                    </Typography>
+                  </Grid>
+                  {/* Next button */}
+                  <Grid item xs={5} sm={4} md={6} align="right">
+                    <Button
+                      id="next"
+                      my={5}
+                      color="primary"
+                      variant="contained"
+                      onClick={next}
+                    >
+                      Next Quiz
+                    </Button>
+                  </Grid>
+                </Grid>
+              </Box>
+
+              {/* Score Display on each question */}
+              <Box mt={4}>
+                <Typography align="center" variant="h5">
+                  Score: {score}
+                </Typography>
+              </Box>
+            </Box>
+          )
+        )
+        :
+        (
+          <>
+            <Box
+              id="main"
+              sx={{
+                bgcolor: "white",
+                boxShadow: "0 10px 15px rgba(0,0,0,0.25), 0 0 6px rgba(0,0,0,0.22)",
+              }}
+            >
+              <Typography color='error' variant="h2" align='center' mb={4} >
+                <b>Quiz App</b>
+              </Typography>
+              <Typography variant="h6" >
+                Total Questions: {questions.length}
+              </Typography>
+              <Typography variant="h6" >
+                Time Given: 4 seconds
+              </Typography>
+              <Typography variant="h6" >
+                Passing Criteria: Above 60%
+              </Typography>
+              <Typography variant="h6" color='error' my={4} sx={{ fontWeight: 'bold' }} >
+                Please provide your details to start the quiz
+              </Typography>
+              <Box mb={4} align='center'>
+                <TextField size='small' id="standard-basic" label="First Name" variant="outlined" />
+              </Box>
+              <Box align='center'>
+                <TextField size='small' id="standard-basic" label="Last Name" variant="outlined" />
+              </Box>
+              {/* <Typography variant="h6" my={4} >
+                Click below to start the Quiz
+              </Typography> */}
+              <Box my={5} sx={{ display: 'flex', justifyContent: 'center' }}>
+                <Button onClick={start} variant='contained' >Click to start Quiz</Button>
+              </Box>
+            </Box>
+          </>
+        )
+      }
     </div>
   );
 }
-
 export default App;
-
-{
-  /* {questions.map((e, i) => (
-          <Box>
-            <Typography variant="body1" key={i}>
-              {e.question}
-            </Typography>
-            <Typography variant="body2">{e.options}</Typography>
-          </Box>
-        ))} */
-}
